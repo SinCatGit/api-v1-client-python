f49b148b63c37feea6b8808f035d1a662cf046f7
SinCatGit <sincat@126.com>
2018-05-08 23:33:11 +0800

    add balance updates api

--- blockchain/v2/receive.py
+++ blockchain/v2/receive.py
@@ -14,6 +14,17 @@ class ReceiveResponse:
         self.callback_url = callback
 
 
+class BalanceUpdateResponse:
+
+    def __init__(self, id, addr, op, confs, callback, onNotification):
+        self.id = id
+        self.addr = addr
+        self.op = op
+        self.confs = confs
+        self.callback = callback
+        self.onNotification = onNotification
+
+
 class LogEntry:
 
     def __init__(self, callback_url, called_at, raw_response, response_code):
@@ -40,6 +51,40 @@ def receive(xpub, callback, api_key):
                                        json_resp['index'],
                                        json_resp['callback'])
     return payment_response
+
+
+def balance_update(address, callback, key, onNotification='KEEP', confs=3, op='ALL'):
+    """Call the '/v2/receive/balance_update' to monitors an address of your choice
+     for received and / or spent payments
+
+    :param str address: The address you would like to monitor
+    :param str callback: The callback URL to be notified when a payment is received
+    :param str key: Your blockchain.info receive payments v2 api key
+    :param str onNotification: The request notification behaviour ('KEEP' | 'DELETE)
+    :param int confs: Optional (Default 3). The number of confirmations the transaction needs to have before a notification is sent.
+    :param str op: Optional (Default 'ALL'). The operation type you would like to receive notifications for ('SPEND' | 'RECEIVE' | 'ALL')
+    :return: an instance of :class: `BalanceUpdateResponse` class
+    """
+
+    params = {
+        'address': address,
+        'key': key,
+        'callback': callback,
+        'onNotification': onNotification,
+        'confs': confs,
+        'op': op
+    }
+    resource = 'v2/receive/balance_update'
+    resp = util.call_api(resource, data=params, base_url='https://api.blockchain.info/')
+    json_resp = json.loads(resp)
+    balance_update_response = BalanceUpdateResponse(
+        id=json_resp['id'],
+        addr=json_resp['addr'],
+        op=json_resp['op'],
+        confs=json_resp['confs'],
+        callback=json_resp['callback'],
+        onNotification=json_resp['onNotification'])
+    return balance_update_response
 
 
 def callback_log(callback, api_key):
